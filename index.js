ee965c6fb29b13be7fa9ba8291d38ab52824cbde
Jozue06 <jozue06@gmail.com>
2018-06-01 20:45:03 -0700

    listing id hashes atleast

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 'use strict';
 
-let app = require('./lib/app.js');
+// let app = require('./lib/app.js');
 
-// Each of these will have event listeners and triggers in them.  Since they're all loaded
-// after app.js, and they all require app.js, they will have access to the events constructor
-let listeners = require('./lib/listeners.js');
-let moreListeners = require('./lib/more-listeners.js');
-let events = require('./lib/events.js');
+// // Each of these will have event listeners and triggers in them.  Since they're all loaded
+// // after app.js, and they all require app.js, they will have access to the events constructor
+// let listeners = require('./lib/listeners.js');
+// let moreListeners = require('./lib/more-listeners.js');
+// let events = require('./lib/events.js');
 
-events.fire();
+// events.fire();
